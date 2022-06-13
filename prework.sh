0331278fd5dd43bae8adb8f246049416f221a54f
Lovely-Pig <1607806186@qq.com>
2022-06-13 16:27:39 +0800

    init

---
+++ prework.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+
+wget https://pjreddie.com/media/files/yolov3.weights
+# wget https://pjreddie.com/media/files/yolov3-tiny.weights
+
+cd data/custom
+python write_img_paths.py
+python voc2yolo.py
+cd ../..
